d21dc926c8e1caa13700c7ce905c6f49a72fdeab
lea <lea1002@hotmail.com>
2023-01-11 16:03:48 +0200

    10.1.23

--- 8.1.23/js/sommeetmoy.js
+++ 8.1.23/js/sommeetmoy.js
@@ -5,8 +5,8 @@ var b = 10 ;
 var c = 3;
 var somme = (a + b + c) 
 somme = 18 ;
-var moy = (a+b+c) /2;
-moy = 9;
+var moy = (a+b+c) /3;
+moy = 6;
 
 var message ="this somm is " + somme + " and this moyenne is " + moy ;
 alert(message)
