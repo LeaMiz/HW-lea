c28d89b66086edaea3856dbee20011d4f2298b02
lea <lea1002@hotmail.com>
2023-01-11 16:00:06 +0200

    10.1.23

---
+++ 10.1.23/js/excercice7.js
@@ -0,0 +1,11 @@
+const students = [];
+var question = prompt("How many are you");
+for (let i = 0; i < 5; i++) { 
+   const student = prompt ("What's your name ?"+ (i+1)) .toLowerCase() ;
+students.push(student);
+}
+students.sort();
+var result = students.join(" , ") ;
+alert(result);
+
+console.log (students)
